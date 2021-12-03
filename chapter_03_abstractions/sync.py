d9d50ea69c1c5a0706d57b8e6ed616b55b3fab78
archepro84 <archepro84@gmail.com>
2021-12-04 00:38:55 +0900

    2021-12-04

---
+++ chapter_03_abstractions/sync.py
@@ -0,0 +1,60 @@
+import hashlib
+import os
+import shutil
+from pathlib import Path
+
+BLOCKSIZE = 65536
+
+
+def sync(source, dest):
+    # 원본 폴더의 자식들을 순회하면서 File Name과 Hash의 Dictionary를 생성한다.
+    source_hashes = read_paths_and_hashes(source)
+    dest_hashes = read_paths_and_hashes(dest)
+
+    # step 2: call functional core
+    actions = determine_actions(source_hashes, dest_hashes, source, dest)
+
+    # imperative shell step 3, apply outputs
+    for action, *paths in actions:
+        if action == "COPY":
+            shutil.copyfile(*paths)
+        if action == "MOVE":
+            shutil.move(*paths)
+        if action == "DELETE":
+            os.remove(paths[0])
+
+
+# File을 Hash하기
+def hash_file(path):
+    hasher = hashlib.sha1()  # sha1로 해시Lib 호출
+    with path.open("rb") as f:
+        buf = f.read(BLOCKSIZE)
+        while buf:
+            hasher.update(buf)
+            buf = f.read(BLOCKSIZE)
+        return hasher.hexdigest()
+
+
+def read_paths_and_hashes(root):
+    hashes = {}
+    for folder, _, files in os.walk(root):
+        for fn in files:
+            hashes[hash_file(Path(folder) / fn)] = fn
+    return hashes
+
+
+def determine_actions(source_hashes, dest_hashes, source_folder, dest_folder):
+    for sha, filename in source_hashes.items():
+        if sha not in dest_hashes:
+            sourcepath = Path(source_folder) / filename
+            destpath = Path(dest_folder) / filename
+            yield "COPY", sourcepath, destpath
+
+        elif dest_hashes[sha] != filename:
+            olddestpath = Path(dest_folder) / dest_hashes[sha]
+            newdestpath = Path(dest_folder) / filename
+            yield "MOVE", olddestpath, newdestpath
+
+    for sha, filename in dest_hashes.items():
+        if sha not in source_hashes:
+            yield "DELETE", dest_folder / filename
--- chapter_03_abstractions/tests/test_sync_01.py
+++ chapter_03_abstractions/tests/test_sync_01.py
@@ -51,3 +51,15 @@ def test_when_a_file_has_been_renamed_in_the_source():
     finally:
         shutil.rmtree(source)
         shutil.rmtree(dest)
+
+
+def test_when_a_file_exists_in_the_source_but_not_the_destination():
+    src_hashes = {'hash1': 'fn1'}
+    dst_hashes = {}
+    expected_actions = [('COPY', '/src/fn1', '/dst/fn1')]
+
+
+def test_when_a_file_has_been_renamed_in_the_source():
+    src_hashes = {'hash1': 'fn1'}
+    dst_hashes = {'hash1': 'fn2'}
+    expected_actions = [('MOVE', '/dst/fn2', '/dst/fn1')]
